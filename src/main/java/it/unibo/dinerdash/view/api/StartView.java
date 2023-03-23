d3ac41c49ca4edcd8b71acffed3401bfabe7e88b
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-03-23 18:11:36 +0100

    [EDITED] Menu view structure

--- src/main/java/it/unibo/dinerdash/view/api/StartView.java
+++ src/main/java/it/unibo/dinerdash/view/api/StartView.java
@@ -7,49 +7,36 @@ import javax.swing.*;
 
 /*
  * Starting Game Menu with 2 buttons
+ * TODO: Aggiungi eventuale immagine di sfondo
  */
 public class StartView extends GenericPanel {
 
-    public StartView(GameView mainFrame) {
+    private JLabel titleLabel;
+    private JButton startButton;
+    private JButton exitButton;
+
+    public StartView(GameView mainFrame) {        
         super(mainFrame);
         setLayout(new GridBagLayout());
-        setPreferredSize(new Dimension(400, 300));
-        
-        // Aggiunge un titolo al centro del pannello
-        JLabel titleLabel = new JLabel("DinerDash");
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 36));
-        add(titleLabel, createGridBagConstraints(0, 0, 2, 1));
-        
-        // Aggiunge il bottone "Start Game"
-        JButton startButton = new JButton("Start Game");
-        startButton.addActionListener(e -> {
-            // Azione del bottone "Start Game"
-            System.out.println("Avvio del gioco...");
-        });
-        add(startButton, createGridBagConstraints(0, 1, 1, 1));
-        
-        // Aggiunge il bottone "Exit"
-        JButton exitButton = new JButton("Exit");
-        exitButton.addActionListener(e -> {
-            // Azione del bottone "Exit"
-            System.exit(0);
-        });
-        add(exitButton, createGridBagConstraints(1, 1, 1, 1));
 
-        setVisible(true);
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(10, 0, 10, 0);
+        
+        titleLabel = new JLabel("Diner Dash");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 60));
+        add(titleLabel, c);
+
+        c.gridy = 1;
+        startButton = new JButton("Start game");
+        startButton.setPreferredSize(new Dimension(300, 40));
+        add(startButton, c);
+
+        c.gridy = 2;        
+        exitButton = new JButton("Exit");
+        exitButton.setPreferredSize(new Dimension(300, 40));
+        add(exitButton, c);
     }
 
-    private static GridBagConstraints createGridBagConstraints(int x, int y, int width, int height) {
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = x;
-        gbc.gridy = y;
-        gbc.gridwidth = width;
-        gbc.gridheight = height;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.insets = new Insets(5, 5, 5, 5); //imposta i margini a 5 pixel su tutti i lati
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
-        return gbc;
-    }
-    
 }
