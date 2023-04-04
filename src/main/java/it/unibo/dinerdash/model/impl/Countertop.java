f4eb65d3f7c5f8b879975f48aa5aaf8b6fe13e80
Donati Matteo <matteo.donati12@studio.unibo.it>
2023-04-04 14:41:27 +0200

    [ADDED] Countertop.java added into the model

---
+++ src/main/java/it/unibo/dinerdash/model/impl/Countertop.java
@@ -0,0 +1,24 @@
+package it.unibo.dinerdash.model.impl;
+
+public class Countertop extends GameEntityImpl {
+
+    private LinkedList<Dish> dishes;    // piatti
+
+    public Countertop(Pair<Integer, Integer> coordinates) {
+        super(coordinates);
+        //TODO Inizializza
+    }
+
+    public void addDish(Dish dish) {
+        //TODO
+    }
+
+    public <Optional<Dish>> takeDish() {
+        //TODO
+    }
+
+    public void clear() {
+        //TODO
+    }
+    
+}
