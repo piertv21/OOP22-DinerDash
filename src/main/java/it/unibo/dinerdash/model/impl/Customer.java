ec18e83468bdf8234244162c26621a95091382d1
MARCO GIORGI <marco.giorgi7@studio.unibo.it>
2023-04-10 09:26:12 +0200

    [MODIFY]Customers ,delete thread, use nanosecond

--- src/main/java/it/unibo/dinerdash/model/impl/Customer.java
+++ src/main/java/it/unibo/dinerdash/model/impl/Customer.java
@@ -7,18 +7,18 @@ import it.unibo.dinerdash.utility.impl.Pair;
 /** 
  * Create a new element "Customer" who will move in the restaurant
  */
-public class Customer extends GameEntityMovableImpl implements Runnable {
+public class Customer extends GameEntityMovableImpl  {
 
     private static final int MOVEMENT_DISTANCE = 5;
-    private static final int TIME_BEFORE_GETANGRY = 16000;
-    private static final int TIME_BEFORE_ORDERING = 4000;
+    private static final long TIME_BEFORE_GETANGRY = 16;
+    private static final long TIME_BEFORE_ORDERING = 4;
     private static final int SPEED = 1;
-    
     private CustomerState state;
     private ModelImpl model;
     private int numClienti;
-    private  Thread trd;
     private int angryCounter=0;
+    private long startThinkTime;
+    private long startAngryTime=0;
     
     public Customer(Pair<Integer, Integer> coordinates, Pair<Integer, Integer> size, ModelImpl model) {
         super(coordinates, size, SPEED);
@@ -45,7 +45,8 @@ public class Customer extends GameEntityMovableImpl implements Runnable {
             if((getPosition().getX()>=this.getDestination().get().getX())&&
             ((getPosition().getY()<=this.getDestination().get().getY()+4)&&
             (getPosition().getY()>=this.getDestination().get().getY()-4)))     //creo una hitbox del tavolo
-            {                                                                           
+            {                          
+                this.startThinkTime=System.nanoTime();                                                 
                     state = CustomerState.THINKING;
                     this.setPosition(this.getDestination().get());
                     this.setActive(false);                                              //cliente pensa, quindi la sua immagine deve sparire       
@@ -53,46 +54,22 @@ public class Customer extends GameEntityMovableImpl implements Runnable {
         }
         else if(state.equals(CustomerState.THINKING))                                          //il cliente pensa a cosa ordinare
         {
-               this.start();
+            if(System.nanoTime()==this.startThinkTime+TIME_BEFORE_ORDERING ) state = CustomerState.ORDERING;
+               
         }
-    }
-
-    @Override
-    public void run() {
-       if(state.equals(CustomerState.THINKING)){
-        try {
-            trd.sleep(TIME_BEFORE_ORDERING);
-            state = CustomerState.ORDERING;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } 
-       }else if(state.equals(CustomerState.LINE)){
-        while(state.equals(CustomerState.LINE)){
-            try {
-                trd.sleep(1000);
-                angryCounter++;
+        else if(state.equals(CustomerState.LINE)){
+            if(this.startAngryTime!=0){
                 if(model.checkFreeTables(this)){
                     // vado a sedermi al tavolo
                     this.state=CustomerState.WALKING;
                     this.model.AssegnoTavolo(this);
-                    angryCounter=0;
+                    
                 }
-                if(angryCounter==TIME_BEFORE_GETANGRY){         //il cliente si arrabbia e se ne va
+                if(System.nanoTime()>= this.startAngryTime+TIME_BEFORE_GETANGRY){         //il cliente si arrabbia e se ne va
                     this.state=CustomerState.ANGRY;
                     model.leaveRestaurant(this);
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } 
-        }
-       }
-    }
-
-    public void start(){
-        if(trd==null)
-        {
-        trd=new Thread (this, "1");
-        trd.start();
+            }else{this.startAngryTime=System.nanoTime();}
         }
     }
 
