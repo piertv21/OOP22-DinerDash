f2bafc6c6db71c4e009311f398c236c4197e7753
MARCO GIORGI <marco.giorgi7@studio.unibo.it>
2023-04-22 12:14:13 +0200

    [ADD] javadoc for controllers method Customer

--- src/main/java/it/unibo/dinerdash/model/api/States/CustomerState.java
+++ src/main/java/it/unibo/dinerdash/model/api/States/CustomerState.java
@@ -1,9 +1,31 @@
 package it.unibo.dinerdash.model.api.States;
-
+/**
+ *  States that can be used for Customer
+ *  {@link #LINE}
+ *  {@link #ANGRY}
+ *  {@link #WALKING}
+ *  {@link #THINKING}
+ *  {@link #ORDERING}
+ */
 public enum CustomerState {
+    /**
+     * customer waiting in line
+     */
     LINE,
+    /**
+     * customer whom waited too much ,and want to leave
+     */
     ANGRY,
+    /**
+     *customer walking to the table
+     */
     WALKING,
+    /**
+     *customer thinking about what to order
+     */
     THINKING,
+    /**
+     *customer wanting to order
+     */
     ORDERING
 }
