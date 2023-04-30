1ed2b5cfb190e8a23d44c0e0d92f3af24494e268
FederB14 <[federico.benzi@studio.unibo.it]>
2023-04-30 10:56:07 +0200

    [CHECK] gradle Table

--- src/main/java/it/unibo/dinerdash/model/api/GameEntities/Table.java
+++ src/main/java/it/unibo/dinerdash/model/api/GameEntities/Table.java
@@ -1,32 +1,75 @@
 package it.unibo.dinerdash.model.api.GameEntities;
 
 import java.util.Optional;
-
 import it.unibo.dinerdash.model.api.States.TableState;
-import it.unibo.dinerdash.utility.impl.Pair;
 
+/**
+  * 
+  * 
+  */
 public interface Table extends GameEntity {
 
-    void setState(TableState TableState);
+    /**
+     * @param tableState
+     */
+    void setState(TableState tableState);
 
+    /**
+     * The state of the table.
+     * 
+     * @return The state of the table
+     */
     TableState getState();
 
+    /**
+     * @param cs
+     */
     void setCustom(Optional<Customer> cs);
 
+    /**
+     * Returns an Optional object containing the customer.
+     * 
+     * @return An Optional object containing the customer
+     */
     Optional<Customer> getCustomer();
 
+    /**
+     * Returns the table number.
+     * 
+     * @return The table number
+     */
     int getTableNumber();
 
+    /**
+     * Sets the number of people seated at the table.
+     * 
+     * @param sppl The number of people seated at the table
+     * @return The number of people seated at the table
+     */
     int setseatedPeople(int sppl);
 
+    /**
+     * Returns the number of people seated at the table.
+     * 
+     * @return The number of people seated at the table
+     */
     int getPeopleSeatedNumber();
 
+    /**
+     * 
+     */
     void startEating();
 
+    /**
+     * 
+     */
     void update();
 
-    Pair<Integer, Integer> getPosition();
-
+    /**
+     * Returns the state of the table in text format.
+     * 
+     * @return The state of the table in text format
+     */
     String getStateInText();
 
 }
