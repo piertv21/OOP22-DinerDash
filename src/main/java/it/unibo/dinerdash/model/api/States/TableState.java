024ddb24d6abf6cd28d5e4bc2f59704617f6d1bc
FederB14 <[federico.benzi@studio.unibo.it]>
2023-04-30 11:26:55 +0200

    [CHECK] gradle tablestate and waitersstate

--- src/main/java/it/unibo/dinerdash/model/api/States/TableState.java
+++ src/main/java/it/unibo/dinerdash/model/api/States/TableState.java
@@ -1,10 +1,33 @@
 package it.unibo.dinerdash.model.api.States;
 
 public enum TableState {
+    /**
+     * Custumer just seated and thinking on what order.
+     */
     THINKING,
+
+    /**
+     * Custumer start ordering food.
+     */
     ORDERING,
+
+    /**
+     * Custumer waiting for his order.
+     */
     WAITING_MEAL,
+
+    /**
+     * Custumer started eating.
+     */
     EATING,
+
+    /**
+     * Custumer finished eating and want to pay.
+     */
     WANTING_TO_PAY,
+
+    /**
+     * Custumer left the table.
+     */
     EMPTY
 }
--- src/main/java/it/unibo/dinerdash/model/api/States/WaitressState.java
+++ src/main/java/it/unibo/dinerdash/model/api/States/WaitressState.java
@@ -1,9 +1,28 @@
 package it.unibo.dinerdash.model.api.States;
 
 public enum WaitressState {
+    /**
+     * Waitress waiting for custumer order.
+     */
     WAITING,
+
+    /**
+     * Waitress his called.
+     */
     CALLING,
+
+    /**
+     * Waitress taking order form custumer.
+     */
     TAKING_DISH,
+
+    /**
+     * Waitress is serving the order.
+     */
     SERVING,
+
+    /**
+     * Waitress is taking money from the custumer.
+     */
     TAKING_MONEY,
 }
