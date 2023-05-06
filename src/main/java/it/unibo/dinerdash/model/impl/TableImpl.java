b23abd7791ac6053d958ea37171afe321ce891f3
FederB14 <[federico.benzi@studio.unibo.it]>
2023-05-06 19:30:52 +0200

    [ADDED] javadoc table

--- src/main/java/it/unibo/dinerdash/model/impl/TableImpl.java
+++ src/main/java/it/unibo/dinerdash/model/impl/TableImpl.java
@@ -2,7 +2,6 @@ package it.unibo.dinerdash.model.impl;
 
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-
 import it.unibo.dinerdash.model.api.gameentities.AbstractGameEntity;
 import it.unibo.dinerdash.model.api.gameentities.Customer;
 import it.unibo.dinerdash.model.api.gameentities.Table;
@@ -10,7 +9,7 @@ import it.unibo.dinerdash.model.api.states.TableState;
 import it.unibo.dinerdash.utility.impl.Pair;
 
 /**
- * 
+ * Create a new element "Table" for people to sit or whait for it to clear
  * 
  */
 public class TableImpl extends AbstractGameEntity implements Table {
@@ -25,9 +24,11 @@ public class TableImpl extends AbstractGameEntity implements Table {
     private int seatedPeople;
 
     /**
-     * @param coordinates
-     * @param size
-     * @param i
+     * Create a new Table.
+     * 
+     * @param coordinates starting table's position
+     * @param size        table's image size (height, width)
+     * @param i           table's number for order
      */
     public TableImpl(final Pair<Integer, Integer> coordinates, final Pair<Integer, Integer> size, final int i) {
         super(coordinates, size);
@@ -38,95 +39,86 @@ public class TableImpl extends AbstractGameEntity implements Table {
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public void setState(final TableState tableState) {
         this.state = tableState;
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public TableState getState() {
         return this.state;
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public void setCustom(final Optional<Customer> cs) {
         this.customer = cs;
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public Optional<Customer> getCustomer() {
         return this.customer;
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public int getTableNumber() {
         return this.tableNumber;
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public void setSeatedPeople(final int peopleAreSeated) {
         this.seatedPeople = peopleAreSeated;
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public int getPeopleSeatedNumber() {
         return this.seatedPeople;
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public void startEating() {
         final var currentTime = System.nanoTime();
-        final var eatingTime = (int) (Math.random() * (MAX_TIME_FOR_EATING - MIN_TIME_FOR_EATING + 1)) + MIN_TIME_FOR_EATING;
+        final var eatingTime = (int) (Math.random() * (MAX_TIME_FOR_EATING - MIN_TIME_FOR_EATING + 1))
+                + MIN_TIME_FOR_EATING;
         this.timeFinishEating = Optional.of(currentTime + TimeUnit.SECONDS.toNanos(eatingTime));
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public void update() {
         if (this.timeFinishEating.isPresent() && System.nanoTime() >= this.timeFinishEating.get()) {
-                state = TableState.WANTING_TO_PAY;
-                this.timeFinishEating = Optional.empty();
+            state = TableState.WANTING_TO_PAY;
+            this.timeFinishEating = Optional.empty();
         }
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public String getStateInText() {
         return switch (this.state) {
@@ -138,9 +130,8 @@ public class TableImpl extends AbstractGameEntity implements Table {
     }
 
     /**
-    * 
-    * 
-    */
+     * {@inheritDoc}
+     */
     @Override
     public Optional<Long> getTimeFinishEating() {
         return this.timeFinishEating;
