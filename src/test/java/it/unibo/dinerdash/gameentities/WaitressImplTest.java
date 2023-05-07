d1c5f1c06f485217166ea8c96a508fdba2bfde9c
FederB14 <[federico.benzi@studio.unibo.it]>
2023-05-07 10:09:01 +0200

    [CREATE] WaitressImplTest

---
+++ src/test/java/it/unibo/dinerdash/gameentities/WaitressImplTest.java
@@ -0,0 +1,65 @@
+package it.unibo.dinerdash.gameentities;
+
+import org.junit.jupiter.api.Test;
+
+public class WaitressImplTest {
+    @Test
+    void testAddOrderForWaitress() {
+
+    }
+
+    @Test
+    void testCheckRightTable() {
+
+    }
+
+    @Test
+    void testCollectMoney() {
+
+    }
+
+    @Test
+    void testGetOrderList() {
+
+    }
+
+    @Test
+    void testGetOrdersNumber() {
+
+    }
+
+    @Test
+    void testGetState() {
+
+    }
+
+    @Test
+    void testGoGetDish() {
+
+    }
+
+    @Test
+    void testIncrementSpeed() {
+
+    }
+
+    @Test
+    void testServeOrder() {
+
+    }
+
+    @Test
+    void testSetState() {
+
+    }
+
+    @Test
+    void testTakeTableOrder() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+}
