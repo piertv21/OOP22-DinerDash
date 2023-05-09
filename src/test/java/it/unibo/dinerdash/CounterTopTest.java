a7af14e87e477e07543b42e48e3829db689fffb8
Matteo Donati <matteo.donati12@studio.unibo.it>
2023-05-09 14:54:10 +0200

    [ADDED] CounterTop and Dish Test file

---
+++ src/test/java/it/unibo/dinerdash/CounterTopTest.java
@@ -0,0 +1,34 @@
+package it.unibo.dinerdash;
+import org.junit.jupiter.api.Test;
+
+public class CounterTopTest {
+    @Test
+    void testAddOrder() {
+
+    }
+
+    @Test
+    void testClear() {
+
+    }
+
+    @Test
+    void testGetNextDishToPrepare() {
+
+    }
+
+    @Test
+    void testSetDishReady() {
+
+    }
+
+    @Test
+    void testTakeDish() {
+
+    }
+
+    @Test
+    void testThereAreDishesToPrepare() {
+
+    }
+}
---
+++ src/test/java/it/unibo/dinerdash/gameentities/DishTest.java
@@ -0,0 +1,10 @@
+package it.unibo.dinerdash.gameentities;
+
+import org.junit.jupiter.api.Test;
+
+public class DishTest {
+    @Test
+    void testGetDishNumber() {
+
+    }
+}
