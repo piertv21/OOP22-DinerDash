b3c0ce22f9a4a298fd23fc64f44465f2476f5d31
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2023-05-13 22:15:43 +0200

    [ADDED] ImageCacher Test

--- src/test/java/it/unibo/dinerdash/ImageReaderTest.java
+++ src/test/java/it/unibo/dinerdash/ImageReaderTest.java
@@ -1,6 +1,7 @@
 package it.unibo.dinerdash;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -20,8 +21,10 @@ final class ImageReaderTest {
 
     @Test
     void testReadImage() {
-        final var image = imageReader.readImage("background.jpg");
-        assertNotEquals(image, null);
+        assertThrows(NullPointerException.class, () -> {
+            imageReader.readImage("randomImageName.jpg");
+        });
+        assertNotNull(imageReader.readImage("background.jpg"));
     }
 
 }
